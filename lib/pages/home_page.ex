d16312309a2cc1c553501926117dd7faac735626
Bugsbunny <micaelnussbaumer@gmail.com>
2026-08-07 23:55:33 +0000

    small fixes

--- lib/pages/home_page.ex
+++ lib/pages/home_page.ex
@@ -8,9 +8,9 @@ defmodule Micaelnussbaumer.HomePage do
   def template(assigns) do
     ~H"""
     <p class="pre-intro">
-      I can help you building software, specially web based applications. Here you can find some my personal blog and a round up of my experience under the `About` section. A link to github gives you some insight into some of my public projects - not only personal but also some commercial open-source work done for different clients.
+      I can help you build software, specially web based applications. Here you can find some my personal blog and a round up of my experience under the `About` section. A link to github gives you some insight into some of my public projects - not only personal but also some commercial open-source work done for different clients.
     </p>
-    <p>Besides github you can find me in some freelancing platforms online.</p>
+    <p style="margin: 20px 5px 0;">Besides github you can find me in some freelancing platforms online.</p>
     <br />
     <hr class="hr-separator-b" />
     <ol class="blog-posts-list">
